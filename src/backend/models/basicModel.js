f6b82a9836e4af82f286e7eb6dd6753e8edbb5c3
Ademílson F. Tonato <ademilsonft@outlook.com>
2016-11-08 13:08:19 -0200

    Corrigindo alinhamento com editorconfig

--- src/backend/models/basicModel.js
+++ src/backend/models/basicModel.js
@@ -1,49 +1,49 @@
 module.exports = {
-    init: function (db, app, path, refName, montaJson) {
-        var ref = db.ref(refName);
+  init: function (db, app, path, refName, montaJson) {
+    var ref = db.ref(refName);
 
-        app.get(path, function (req, res) {
-            ref.once('value', function (snapshot) {
-                res.send(snapshot.val());
-            });
-        });
+    app.get(path, function (req, res) {
+      ref.once('value', function (snapshot) {
+        res.send(snapshot.val());
+      });
+    });
 
-        app.get(path + "/:key", function (req, res) {
-            ref.child(req.params.key).once('value', function (snap) {
-                var obj = snap.val();
+    app.get(path + '/:key', function (req, res) {
+      ref.child(req.params.key).once('value', function (snap) {
+        var obj = snap.val();
 
-                if (obj == null) {
-                    res.status(404).send();
-                } else {
-                    res.send(obj);
-                }
-            });
-        });
+        if (obj == null) {
+          res.status(404).send();
+        } else {
+          res.send(obj);
+        }
+      });
+    });
 
-        app.post(path, function (req, res) {
-            ref.push(montaJson(req.body)).then((snap) => {
-                res.status(201).send(snap.key);
-            });
-        });
+    app.post(path, function (req, res) {
+      ref.push(montaJson(req.body)).then((snap) => {
+        res.status(201).send(snap.key);
+      });
+    });
 
-        app.put(path + "/:key", function (req, res) {
-            ref.once('value', function (snap) {
-                if (!snap.hasChild(req.params.key)) {
-                    res.status(404).send();
-                } else {
-                    ref.child(req.params.key).set(montaJson(req.body)).then(() => res.status(200).send())
-                }
-            });
-        });
+    app.put(path + '/:key', function (req, res) {
+      ref.once('value', function (snap) {
+        if (!snap.hasChild(req.params.key)) {
+          res.status(404).send();
+        } else {
+          ref.child(req.params.key).set(montaJson(req.body)).then(() => res.status(200).send())
+        }
+      });
+    });
 
-        app.delete(path + "/:key", function (req, res) {
-            ref.once('value', function (snap) {
-                if (!snap.hasChild(req.params.key)) {
-                    res.status(404).send();
-                } else {
-                    ref.child(req.params.key).remove().then(() => res.status(200).send())
-                }
-            });
-        });
-    }
+    app.delete(path + '/:key', function (req, res) {
+      ref.once('value', function (snap) {
+        if (!snap.hasChild(req.params.key)) {
+          res.status(404).send();
+        } else {
+          ref.child(req.params.key).remove().then(() => res.status(200).send())
+        }
+      });
+    });
+  }
 }
--- src/backend/models/categoria.js
+++ src/backend/models/categoria.js
@@ -1,51 +1,51 @@
 var path = '/categoria';
 
 module.exports = {
-    init: function (db, app) {
-        var cat = db.ref('/categoria');
+  init: function (db, app) {
+    var cat = db.ref('/categoria');
 
-        app.get(path, function (req, res) {
-            cat.once('value', function (snapshot) {
-                res.send(snapshot.val());
-            });
-        });
+    app.get(path, function (req, res) {
+      cat.once('value', function (snapshot) {
+        res.send(snapshot.val());
+      });
+    });
 
-        app.get(path + "/:key", function (req, res) {
-            cat.child(req.params.key).once('value', function (snap) {
-                var obj = snap.val();
+    app.get(path + '/:key', function (req, res) {
+      cat.child(req.params.key).once('value', function (snap) {
+        var obj = snap.val();
 
-                if (obj == null) {
-                    res.status(404).send();
-                } else {
-                    res.send(obj);
-                }
-            });
-        });
+        if (obj == null) {
+          res.status(404).send();
+        } else {
+          res.send(obj);
+        }
+      });
+    });
 
-        app.post(path, function (req, res) {
-            cat.push({ nome: req.body.nome }).then((snap) => {
-                res.status(201).send(snap.key);
-            });
-        });
+    app.post(path, function (req, res) {
+      cat.push({ nome: req.body.nome }).then((snap) => {
+        res.status(201).send(snap.key);
+      });
+    });
 
-        app.put(path + "/:key", function (req, res) {
-            cat.once('value', function (snap) {
-                if (!snap.hasChild(req.params.key)) {
-                    res.status(404).send();
-                } else {
-                    cat.child(req.params.key).set({ nome: req.body.nome }).then(() => res.status(200).send())
-                }
-            });
-        });
+    app.put(path + '/:key', function (req, res) {
+      cat.once('value', function (snap) {
+        if (!snap.hasChild(req.params.key)) {
+          res.status(404).send();
+        } else {
+          cat.child(req.params.key).set({ nome: req.body.nome }).then(() => res.status(200).send())
+        }
+      });
+    });
 
-        app.delete(path + "/:key", function (req, res) {
-            cat.once('value', function (snap) {
-                if (!snap.hasChild(req.params.key)) {
-                    res.status(404).send();
-                } else {
-                    cat.child(req.params.key).remove().then(() => res.status(200).send())
-                }
-            });
-        });
-    }
+    app.delete(path + '/:key', function (req, res) {
+      cat.once('value', function (snap) {
+        if (!snap.hasChild(req.params.key)) {
+          res.status(404).send();
+        } else {
+          cat.child(req.params.key).remove().then(() => res.status(200).send())
+        }
+      });
+    });
+  }
 }
