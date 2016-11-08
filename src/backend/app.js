7e9b142da80194c79ba768ba85902b8e801fa4a7
Rodrigo Cullmann <rodkulman@gmail.com>
2016-11-08 00:39:20 -0200

    CRUD de categoria funcionando

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,47 +1,24 @@
 var express = require('express');
 var firebase = require('firebase');
+var bodyParser = require('body-parser');
 
-firebase.initializeApp({
-    databaseURL: "https://objeto-relacional.firebaseio.com/"
-});
+var categoria = require('./models/categoria.js');
+
+var config = {
+    apiKey: "AIzaSyD5ia4TuqxEhLpeAgAflbTd_Tr5HkBcBb0",
+    authDomain: "objeto-relacional.firebaseapp.com",
+    databaseURL: "https://objeto-relacional.firebaseio.com",
+    storageBucket: "objeto-relacional.appspot.com",
+    messagingSenderId: "523318008117"
+};
+
+firebase.initializeApp(config);
 
 var app = express();
 var db = firebase.database();
 
-app.get("/categoria", function (req, res) {
-    var cat = db.ref('/categoria');
+app.use(bodyParser.json());
 
-    cat.on('value', function(snapshot) {
-        res.send(snapshot.val())
-        cat.off('value');
-
-    }, function(err){});
-});
-
-app.get("/categoria/:key", function(req, res){
-    var cat = db.ref('/categoria');
-
-    cat.child(req.params.key).on('value', function(snap){
-        var obj = snap.val();
-
-        if (obj == null){
-            res.status(404).send('Sorry!');
-        }else{
-            res.send(obj);
-        }        
-    }, function(err){});
-});
-
-app.post("/categoria", function (req, res) {
-    
-});
-
-app.put("/categoria", function (req, res) {
-    
-});
-
-app.delete("/categoria", function (req, res) {
-    
-});
+categoria.init(db, app);
 
 app.listen(8080);
