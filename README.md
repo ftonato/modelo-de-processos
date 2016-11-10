4b3848fbd587263849104e2e35c5f7a94fdaa18b
Rodrigo Cullmann <rodkulman@gmail.com>
2016-11-10 03:49:27 -0200

    Todo CRUD criado. Só não testei tudo e nem documentei, tem que fazer isso. :/

--- README.md
+++ README.md
@@ -7,21 +7,25 @@ Categorias
 ------------
 `GET /categoria` Busca todas as categorias
 ```javascript
+// JSON recebido
 {"chave":{"nome":"string"}}
 ```
 
 `GET /categoria/<chave>` Busca uma categoria especifica
 ```javascript
+// JSON recebido
 {"nome":"string"}
 ```
 
 `POST /categoria` Cria categoria e retorna a chave criada
 ```javascript
+// JSON enviado
 {"nome":"string"}
 ```
 
 `PUT /categoria/<chave>` Atualiza uma categoria
 ```javascript
+// JSON enviado
 {"nome":"string"}
 ```
 
---
+++ src/backend/models/metasGen.js
@@ -0,0 +1,83 @@
+const path = '/metasGenericas';
+
+function validateBody(reqBody, db, allGoodCallback, notGoodCallback) {
+    if (reqBody.nome == null) {
+        notGoodCallback('nome nao informado');
+    } else if (reqBody.descricao == null) {
+        notGoodCallback('descricao nao informada');
+    } else if (reqBody.sigla == null) {
+        notGoodCallback('sigla nao informada');
+    } else if (reqBody.nivelCap == null) {
+        notGoodCallback('Chave do nivel de capacidade nao informada');
+    } else {
+        db.ref('/nivelCap').once('value', function (snap) {
+            if (!snap.hasChild(reqBody.nivelCap)) {
+                notGoodCallback('Chave de nivel de capacidade nao existe');
+            } else {
+                allGoodCallback();
+            }
+        });
+    }
+}
+
+function montaJson(reqBody) {
+    return { nome: reqBody.nome, descricao: reqBody.descricao, sigla: reqBody.sigla, nivelCap: reqBody.nivelCap };
+}
+
+module.exports = {
+    init: function (db, app) {
+        var ref = db.ref('/metasGen');
+
+        app.get(path, function (req, res) {
+            ref.once('value', function (snapshot) {
+                res.send(snapshot.val());
+            });
+        });
+
+        app.get(path + "/:key", function (req, res) {
+            ref.child(req.params.key).once('value', function (snap) {
+                var obj = snap.val();
+
+                if (obj == null) {
+                    res.status(404).send();
+                } else {
+                    res.send(obj);
+                }
+            });
+        });
+
+        app.post(path, function (req, res) {
+            validateBody(req.body, db, function () {
+                ref.push(montaJson(req.body)).then((snap) => {
+                    res.status(201).send(snap.key);
+                });
+            }, function (error) {
+                res.status(400).send(error);
+            });
+        });
+
+        app.put(path + "/:key", function (req, res) {
+            ref.once('value', function (snap) {
+                if (!snap.hasChild(req.params.key)) {
+                    res.status(404).send();
+                } else {
+                    validateBody(req.body, db, function () {
+                        ref.child(req.params.key).set(montaJson(req.body)).then(() => res.status(200).send());
+                    }, function (error) {
+                        res.status(400).send(error);
+                    });
+                }
+            });
+        });
+
+        app.delete(path + "/:key", function (req, res) {
+            ref.once('value', function (snap) {
+                if (!snap.hasChild(req.params.key)) {
+                    res.status(404).send();
+                } else {
+                    ref.child(req.params.key).remove().then(() => res.status(200).send())
+                }
+            });
+        });
+    }
+}
