209142d49ee19b38f99d4e7dc04e2f274869087f
Rodrigo Cullmann <rodkulman@gmail.com>
2016-11-09 23:50:17 -0200

    Atualizado documentação da API

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # modelo-de-processos
+
+Acessso a API
+=============
+
+Categorias
+------------
+`GET /categoria` Busca todas as categorias
+```javascript
+{"chave":{"nome":"string"}}
+```
+
+`GET /categoria/<chave>` Busca uma categoria especifica
+```javascript
+{"nome":"string"}
+```
+
+`POST /categoria` Cria categoria e retorna a chave criada
+```javascript
+{"nome":"string"}
+```
+
+`PUT /categoria/<chave>` Atualiza uma categoria
+```javascript
+{"nome":"string"}
+```
+
+`DELETE /categoria/<chave>` Exclui uma categoria especifica 
--- src/backend/app.js
+++ src/backend/app.js
@@ -2,9 +2,6 @@ var express = require('express');
 var firebase = require('firebase');
 var bodyParser = require('body-parser');
 
-// var categoria = require('./models/categoria.js');
-// var nivelCapacidade = require('./models/nivelCapacidade.js');
-
 var basicModel = require('./models/basicModel.js');
 
 var config = {
@@ -21,9 +18,6 @@ var app = express();
 var db = firebase.database();
 
 app.use(bodyParser.json());
-
-// categoria.init(db, app);
-// nivelCapacidade.init(db, app);
 
 basicModel.init(db, app, '/categoria', '/categoria', function (body) { return { nome: body.nome } });
 basicModel.init(db, app, '/nivelCapacidade', '/nivelCap', function (body) { return { nome: body.nome, descricao: body.descricao, sigla: body.sigla } });
