a239e162b7dd4feed997de7b2b4140f446290eb8
Ademílson F. Tonato <ademilsonft@outlook.com>
2016-11-18 13:24:07 -0200

    Adicionar estrutura do frontend

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<!-- <template>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <a class="navbar-brand" href="#">BIRL</a>
+    <ul class="nav navbar-nav">
+      <li>
+        <a v-link="{ name: 'dashboard' }">Dashboard</a>
+      </li>
+      <li>
+        <a v-link="{ name: 'users' }">Usuários</a>
+      </li>
+    </ul>
+  </nav>
+</template> -->
+<template>
+  <div class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar">
+            </span>
+            <span class="icon-bar">
+            </span>
+            <span class="icon-bar">
+            </span>
+          </button>
+          <a class="navbar-brand" href="../index.html">
+            <span>MP</span>
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="modelo.html">Modelo</a>
+            </li>
+            <li>
+              <a href="nivel_capacidade.html">Nível Capacidade</a>
+            </li>
+            <li>
+              <a href="nivel_maturidade.html">Nível Maturidade</a>
+            </li>
+            <li>
+              <a href="categoria.html">Categoria</a>
+            </li>
+            <li>
+              <a href="produto_trabalho.html">Produtos Trabalho</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div> <!-- / .navbar -->
+</template>
