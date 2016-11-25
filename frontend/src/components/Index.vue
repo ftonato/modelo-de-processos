6879d016926ec1a044b84d2ba58162f76904c7d6
Ademílson F. Tonato <contato@admcode.com.br>
2016-11-24 23:54:19 -0200

    Novas funcionalidades e remoção de dependências desnecessárias (#13)
    * Adicionar .vscode ao editorconfig
    
    * Remover arquivo .vscode
    
    * Adicionar uma descrição básicaa a pagina inicial do projeto
    
    * Adicionar link de download para os Modelos
    
    * Adicionar validacao das metas especificas para o cadastro de praticas especificas
    
    * Adicionar nome e descricao ao projeto
    
    * Remover README desnecessario
    
    * Remover comentarios desnecessarios

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-  <h1>Bem vindo ao MP</h1>
+  <div class="text-center">
+    <h1>Bem vindo a central de MP</h1>
+    <p>Um modelo de processo de desenvolvimento de software, ou simplesmente modelo de processo, pode ser visto como uma representação, ou abstração dos objetos e atividades envolvidas no processo de software. Além disso, oferece uma forma mais abrangente e fácil de representar o gerenciamento de processo de software e consequentemente o progresso do projeto.</p>
   </div>
 </template>
 
 <script>
   export default { }
 </script>
+<style scoped>
+  div {
+    width: 80%;
+    margin: 0 auto;
+  }
+  h1, p {
+    padding-top: 8rem;
+  }
+  p {
+    font-size: 2rem;
+  }
+</style>
